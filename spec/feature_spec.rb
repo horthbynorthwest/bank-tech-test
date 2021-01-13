7dd20a8cfa80f5dcde39ff52e463142ec1ec01bc
Georgie Horth <georgiehorth@gmail.com>
2021-01-13 13:25:34 +0000

    Added feature_spec which shows how the two classes work together with no doubling. Date is still stubbed so tests continue to pass in the future

---
+++ spec/feature_spec.rb
@@ -0,0 +1,15 @@
+require 'bank_account'
+require 'statement'
+
+describe 'example transaction' do
+
+  it 'programme displays statement correctly' do
+    @bank_account = BankAccount.new
+    allow(@bank_account).to receive(:date) { '11/01/2021' }
+    @bank_account.deposit(1000)
+    @bank_account.deposit(2000)
+    @bank_account.withdraw(500)
+    msg = "date || credit || debit || balance\n11/01/2021 || 0.00 || 500.00 || 2500.00\n11/01/2021 || 2000.00 || 0.00 || 3000.00\n11/01/2021 || 1000.00 || 0.00 || 1000.00\n"
+    expect { @bank_account.print_statement }.to output(msg).to_stdout
+  end
+end
